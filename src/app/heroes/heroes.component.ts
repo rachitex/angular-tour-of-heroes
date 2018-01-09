1a74c4ce6f026cbace5e354cd3fc4f95fd50bb31
moon <rachitmishra5594@gmail.com>
2018-01-09 23:55:51 +0530

    Edited getHeroes() method to get Obersvable<Hero[]> instead of Hero[]

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -20,7 +20,8 @@ export class HeroesComponent implements OnInit {
   constructor(private heroService: HeroService) { }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 // OnInit interface has ngOnInit() and ngOnDestroy() for Life hooks of components.
   ngOnInit() {
