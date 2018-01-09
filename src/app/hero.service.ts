f23e68462e5265177d5fde5d84c86ff5ee2ac0fe
moon <rachitmishra5594@gmail.com>
2018-01-10 00:16:08 +0530

    Imported MessageService in hero.service.ts

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,7 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
