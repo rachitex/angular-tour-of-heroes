119f4854afc29b098543a09c13aaf83991a5b465
moon <rachitmishra5594@gmail.com>
2018-01-10 07:41:49 +0530

    Imported RouterModule  and Added  NgModule.exports array with RouterModule in it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
-
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
