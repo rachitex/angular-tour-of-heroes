a941cb610bb54ccb58250c56a0c2a7a11efcdb47
moon <rachitmishra5594@gmail.com>
2018-01-10 22:31:12 +0530

    Added constructor and ngOnInit life cycle hook

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -13,9 +13,20 @@ import { HeroService } from '../hero.service';
 export class HeroDetailComponent implements OnInit {
 
   @Input() hero: Hero;
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
   }
 
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,3 +1,7 @@
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Hero } from 'app/hero';
+import { HeroService } from 'app/hero.service';
+
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
 
