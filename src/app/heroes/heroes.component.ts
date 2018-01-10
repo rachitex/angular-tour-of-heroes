9345179945d95bffcd5f4ac69a44899437bf160d
moon <rachitmishra5594@gmail.com>
2018-01-10 22:15:01 +0530

    Imported ActivatedRoute, Location and HeroService in HeroDetailComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,34 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero'; // Importing Hero class from hero.ts file.
-import { HeroService } from '../hero.service';
-
-@Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
-})
 export class HeroesComponent implements OnInit {
-/*
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
-*/
-  selectedHero: Hero;
   heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
 
-  getHeroes(): void {
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
-  }
-// OnInit interface has ngOnInit() and ngOnDestroy() for Life hooks of components.
   ngOnInit() {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
