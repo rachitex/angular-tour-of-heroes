4435cf6783f552d318bc33ff944340694b7ff43e
moon <rachitmishra5594@gmail.com>
2018-01-16 22:00:17 +0530

    Updated getHeroes() method in hero.service.ts

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,10 +16,9 @@ export class HeroService {
     private messageService: MessageService
   ) { }
 
-  getHeroes(): Observable<Hero[]> {
-    // Todo: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+  /** GET heroes from the server */
+  getHeroes (): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
